9fe186159151b8382f9c24e6138bac8bd1f858a7
alesiani <alesiani@c506.ant-net>
2022-06-14 09:14:48 +0200

    init file

--- pdebench/__init__.py
+++ pdebench/__init__.py
@@ -22,4 +22,4 @@ Citations
 
 __version__ = "0.0.1"
 __author__ = 'Makoto Takamoto, Timothy Praditia, Raphael Leiteritz, Dan MacKinlay, Francesco Alesiani, Dirk Pflüger, Mathias Niepert'
-__credits__ = 'NEC labs Europe, University of Stuttgart, CSIRO’s Data61'
+__credits__ = 'NEC labs Europe, University of Stuttgart, CSIRO''s Data61'
