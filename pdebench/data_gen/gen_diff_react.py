535d9f147ded7b42bb265daafc06852b67bc3283
Timothy Praditia <timothypraditia89@gmail.com>
2022-08-04 13:55:36 +0200

    Added time measurement for data generation of 2d diffusion-reaction, diffusion-sorption, and radial dam break scenarios, fixed bugs in NLE data generation utils script

--- pdebench/data_gen/gen_diff_react.py
+++ pdebench/data_gen/gen_diff_react.py
@@ -8,6 +8,8 @@ import dotenv
 # e.g. HPC versus local laptop
 dotenv.load_dotenv()
 
+import time
+
 # or if the environment variables will be fixed for all executions, we can hard-code the environment variables like this:
 num_threads = "4"
 
@@ -32,37 +34,38 @@ from uploader import dataverse_upload
 
 log = logging.getLogger(__name__)
 
-def simulator(config, i, data_f):
+def simulator(config, i):
     
     from src import sim_diff_react
     
     config.sim.seed = i
     log.info(f"Starting seed {i}")
+    start_time = time.time()
     sim_obj = sim_diff_react.Simulator(**config.sim)
     data_sample = sim_obj.generate_sample()
+    duration = time.time() - start_time
+    log.info(f"Seed {config.sim.seed} took {duration} to finish")
 
     seed_str = str(i).zfill(4)
     
-    ## Chunking for compression and data access
-    ## https://docs.h5py.org/en/stable/high/dataset.html#chunked-storage
-    ## should be by batch and less than 1MB
-    ## lzf compression for float32 is kind of pointless though.
-    data_f.create_dataset(
-        f"{seed_str}/data",
-        data=data_sample,
-        dtype="float32",
-        compression="lzf"
-    )
-    data_f.create_dataset(
-        f"{seed_str}/grid/x", data = sim_obj.x, dtype="float32", compression="lzf"
-    )
-    data_f.create_dataset(
-        f"{seed_str}/grid/y", data=sim_obj.y, dtype="float32", compression="lzf" 
-    )
-    data_f.create_dataset(
-        f"{seed_str}/grid/t", data=sim_obj.t, dtype="float32", compression="lzf" 
-    )
-    data_f.attrs[f"{seed_str}/config"] = OmegaConf.to_yaml(config)
+    with h5py.File(utils.expand_path(config.output_path), "a") as data_f:
+        ## Chunking for compression and data access
+        ## https://docs.h5py.org/en/stable/high/dataset.html#chunked-storage
+        ## should be by batch and less than 1MB
+        ## lzf compression for float32 is kind of pointless though.
+        data_f.create_dataset(
+            f"{seed_str}/data", data=data_sample, dtype="float32", compression="lzf"
+        )
+        data_f.create_dataset(
+            f"{seed_str}/grid/x", data = sim_obj.x, dtype="float32", compression="lzf"
+        )
+        data_f.create_dataset(
+            f"{seed_str}/grid/y", data=sim_obj.y, dtype="float32", compression="lzf" 
+        )
+        data_f.create_dataset(
+            f"{seed_str}/grid/t", data=sim_obj.t, dtype="float32", compression="lzf" 
+        )
+        data_f.attrs[f"{seed_str}/config"] = OmegaConf.to_yaml(config)
 
 
 @hydra.main(config_path="configs/", config_name="diff-react")
@@ -94,12 +97,10 @@ def main(config: DictConfig):
     num_samples_init = 0
     num_samples_final = 1000
 
-    with h5py.File(utils.expand_path(config.output_path), "w") as data_f:
-
-        pool = mp.Pool(mp.cpu_count())
-        seed = np.arange(num_samples_init, num_samples_final)
-        seed = seed.tolist()
-        pool.starmap(simulator, zip(repeat(config), seed, data_f))
+    pool = mp.Pool(mp.cpu_count())
+    seed = np.arange(num_samples_init, num_samples_final)
+    seed = seed.tolist()
+    pool.starmap(simulator, zip(repeat(config), seed))
 
     if config.upload:
         dataverse_upload(
