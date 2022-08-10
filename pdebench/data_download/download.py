a392811c7a0336424bffb2e3501994bcdb7033df
leiterrl <leiterrl@ipvs.uni-stuttgart.de>
2022-08-10 19:10:01 +0200

    simplify download utility script

--- pdebench/data_download/download.py
+++ pdebench/data_download/download.py
@@ -1,14 +1,10 @@
 import os
-import dotenv
-
-dotenv.load_dotenv()
 
 import hydra
 from hydra.utils import get_original_cwd
 from omegaconf import DictConfig
 import logging
 
-import glob
 from pyDataverse.api import NativeApi, DataAccessApi
 from pyDaRUS import Dataset
 from easyDataverse.core.downloader import download_files
@@ -19,7 +15,7 @@ log = logging.getLogger(__name__)
 @hydra.main(config_path="config/", config_name="config")
 def main(config: DictConfig):
     """
-    use config specifications to generate dataset
+    use config specifications to download files from a dataset
     """
 
     # Imports should be nested inside @hydra.main to optimize tab completion
@@ -29,21 +25,14 @@ def main(config: DictConfig):
 
     os.chdir(get_original_cwd())
 
-    path_to_data = os.path.join(os.path.abspath("../data"), config.args.filename)
-
-    file_list = sorted(glob.glob(path_to_data + "/*.h5"))
-    doi = "doi:10.18419/darus-2986"
-
     # Extract dataset from the given DOI
     dataset = Dataset()
-    setattr(dataset, "p_id", doi)
+    setattr(dataset, "p_id", config.args.dataset_id)
 
     # Extract file list contained in the dataset
-    api = NativeApi(os.getenv("DATAVERSE_URL"), os.getenv("DATAVERSE_API_TOKEN"))
-    data_api = DataAccessApi(
-        os.getenv("DATAVERSE_URL"), os.getenv("DATAVERSE_API_TOKEN")
-    )
-    dv_dataset = api.get_dataset(doi)
+    api = NativeApi(config.args.dataverse_url)
+    data_api = DataAccessApi(config.args.dataverse_url)
+    dv_dataset = api.get_dataset(config.args.dataset_id)
     files_list = dv_dataset.json()["data"]["latestVersion"]["files"]
 
     # Compile list of files that matches the desired filename
@@ -53,12 +42,8 @@ def main(config: DictConfig):
             files.append(file)
 
     # Download the files
-    download_files(data_api, dataset, files, os.path.abspath("../data/"))
+    download_files(data_api, dataset, files, os.path.abspath(config.args.data_folder))
 
-    return
-
-
-import os
 
 if __name__ == "__main__":
     main()
