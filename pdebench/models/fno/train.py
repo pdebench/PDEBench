044a823d48d5d2d13fbc4928c6bb5e12ddff045e
Timothy Praditia <timothypraditia89@gmail.com>
2022-10-28 14:25:45 +0200

    Changed target time step for 'single' mode prediction for consistency, changed validation target to exclude initial time steps

--- pdebench/models/fno/train.py
+++ pdebench/models/fno/train.py
@@ -238,7 +238,7 @@ def run_training(if_training,
 
             if training_type in ['single']:
                 x = xx[..., 0 , :]
-                y = yy[..., t_train:t_train+1 , :]
+                y = yy[..., t_train-1:t_train, :]
                 pred = model(x, grid)
                 _batch = yy.size(0)
                 loss += loss_fn(pred.reshape(_batch, -1), y.reshape(_batch, -1))
@@ -279,12 +279,13 @@ def run_training(if_training,
             
                         val_l2_step += loss.item()
                         _batch = yy.size(0)
-                        _yy = yy[..., :t_train, :]
-                        val_l2_full += loss_fn(pred.reshape(_batch, -1), _yy.reshape(_batch, -1)).item()
+                        _pred = pred[..., initial_step:t_train, :]
+                        _yy = yy[..., initial_step:t_train, :]
+                        val_l2_full += loss_fn(_pred.reshape(_batch, -1), _yy.reshape(_batch, -1)).item()
 
                     if training_type in ['single']:
                         x = xx[..., 0 , :]
-                        y = yy[..., t_train:t_train+1 , :]
+                        y = yy[..., t_train-1:t_train, :]
                         pred = model(x, grid)
                         _batch = yy.size(0)
                         loss += loss_fn(pred.reshape(_batch, -1), y.reshape(_batch, -1))
