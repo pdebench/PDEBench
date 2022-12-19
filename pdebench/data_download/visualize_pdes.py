9afc60c190e359af778cf0ef20c32f8423f3c8bb
nle18370 <makoto.takamoto@neclab.eu>
2022-12-19 16:10:59 +0100

    modify visualization_pdes.py by MT@19122022

--- pdebench/data_download/visualize_pdes.py
+++ pdebench/data_download/visualize_pdes.py
@@ -150,36 +150,270 @@ def visualize_swe(path, seed=None):
     print("saved")
 
 
-def visualize_burgers():
-    pass
+def visualize_burgers(path, param=None):
+    """
+    This function animates the Burgers equation
+
+    Args:
+    path : path to the desired file
+    param: PDE parameter to be visualized
+    """
+
+    # Read the h5 file and store the data
+    if param is not None:
+        flnm = "1D_Burgers_Sols_Nu" + str(param) + ".hdf5"
+        assert os.path.isfile(path + flnm), 'no such file! '+path + flnm
+    else:
+        flnm = "1D_Burgers_Sols_Nu0.01.hdf5"
+
+    nb = 0
+    with h5py.File(os.path.join(path, flnm), "r") as h5_file:
+        xcrd = np.array(h5_file["x-coordinate"], dtype=np.float32)
+        data = np.array(h5_file["tensor"], dtype=np.float32)[nb]  # (batch, t, x, channel) --> (t, x, channel)
+
+    # Initialize plot
+    fig, ax = plt.subplots()
+
+    # Store the plot handle at each time step in the 'ims' list
+    ims = []
+
+    for i in range(data.shape[0]):
+        im,  = ax.plot(xcrd, data[i].squeeze(), animated=True)
+        if i == 0:
+            ax.plot(xcrd, data[i].squeeze())  # show an initial one first
+        ax.plot
+        ims.append([im])
+
+    # Animate the plot
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+
+    writer = animation.PillowWriter(fps=15, bitrate=1800)
+    ani.save("movie.gif", writer=writer)
+    print("saved")
 
 
-def visualize_advection():
-    pass
+def visualize_advection(path, param=None):
+    """
+    This function animates the Burgers equation
+
+    Args:
+    path : path to the desired file
+    param: PDE parameter to be visualized
+    """
+
+    # Read the h5 file and store the data
+    if param is not None:
+        flnm = "1D_Advection_Sols_beta" + str(param) + ".hdf5"
+        assert os.path.isfile(path + flnm), 'no such file! '+ path + flnm
+    else:
+        flnm = "1D_Advection_Sols_beta0.4.hdf5"
+
+    nb = 0
+    with h5py.File(os.path.join(path, flnm), "r") as h5_file:
+        xcrd = np.array(h5_file["x-coordinate"], dtype=np.float32)
+        data = np.array(h5_file["tensor"], dtype=np.float32)[nb]  # (batch, t, x, channel) --> (t, x, channel)
+
+    # Initialize plot
+    fig, ax = plt.subplots()
+
+    # Store the plot handle at each time step in the 'ims' list
+    ims = []
+    for i in range(data.shape[0]):
+        im,  = ax.plot(xcrd, data[i].squeeze(), animated=True)
+        if i == 0:
+            ax.plot(xcrd, data[i].squeeze())  # show an initial one first
+        ax.plot
+        ims.append([im])
+
+    # Animate the plot
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+
+    writer = animation.PillowWriter(fps=15, bitrate=1800)
+    ani.save("movie.gif", writer=writer)
+    print("saved")
 
 
-def visualize_1d_cfd():
-    pass
+def visualize_1d_cfd(path, param=None):
+    """
+    This function animates the Burgers equation
+
+    Args:
+    path : path to the desired file
+    param: PDE parameter to be visualized
+    """
+
+    # Read the h5 file and store the data
+    if param is not None:
+        assert len(param) == 4, 'param should include type,eta,zeta,boundary as list'
+        flnm = "1D_CFD_" + str(param[0]) + "_Eta" + str(param[1]) + '_Zeta' + str(param[2]) +"_" + str(param[3]) + "_Train.hdf5"
+        assert os.path.isfile(path + flnm), 'no such file! '+ path + flnm
+    else:
+        flnm = "1D_CFD_Rand_Eta1.e-8_Zeta1.e-8_periodic_Train.hdf5"
+
+    nb = 0
+    with h5py.File(os.path.join(path, flnm), "r") as h5_file:
+        xcrd = np.array(h5_file["x-coordinate"], dtype=np.float32)
+        dd = np.array(h5_file["density"], dtype=np.float32)[nb]  # (batch, t, x, channel) --> (t, x, channel)
+
+    # Initialize plot
+    fig, ax = plt.subplots()
+
+    # Store the plot handle at each time step in the 'ims' list
+    ims = []
+    ax.set_title('density')
+    for i in range(dd.shape[0]):
+        im,  = ax.plot(xcrd, dd[i].squeeze(), animated=True)
+        if i == 0:
+            ax.plot(xcrd, dd[i].squeeze())  # show an initial one first
+        ax.plot
+        ims.append([im])
+
+    # Animate the plot
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+
+    writer = animation.PillowWriter(fps=15, bitrate=1800)
+    ani.save("movie.gif", writer=writer)
+    print("saved")
 
 
-def visualize_2d_cfd():
-    pass
+def visualize_2d_cfd(path, param=None):
+    # Read the h5 file and store the data
+    if param is not None:
+        assert len(param) == 6, 'param should include type,M,eta,zeta,boundary, resolution as list'
+        flnm = "2D_CFD_" + str(param[0]) + "_M" + str(param[1]) + "_Eta" + str(param[2]) + '_Zeta' + str(param[3]) + "_" + str(param[4]) + "_" + str(param[5]) + "_Train.hdf5"
+        assert os.path.isfile(path + flnm), 'no such file! '+ path + flnm
+    else:
+        flnm = "2D_CFD_Rand_M0.1_Eta1e-8_Zeta1e-8_periodic_512_Train.hdf5"
+
+    nb = 0
+    with h5py.File(os.path.join(path, flnm), "r") as h5_file:
+        dd = np.array(h5_file["density"], dtype=np.float32)[nb]  # (batch, t, x, y, channel) --> (t, x, y, channel)
+
+    # Initialize plot
+    fig, ax = plt.subplots()
+
+    # Store the plot handle at each time step in the 'ims' list
+    ims = []
+    ax.set_title('density')
+    for i in range(dd.shape[0]):
+        im = ax.imshow(dd[i].squeeze(), animated=True)
+        ims.append([im])
+
+    # Animate the plot
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+
+    writer = animation.PillowWriter(fps=15, bitrate=1800)
+    ani.save("movie.gif", writer=writer)
+    print("saved")
 
 
-def visualize_3d_cfd():
-    pass
+def visualize_3d_cfd(path, param=None):
+    # Read the h5 file and store the data
+    if param is not None:
+        assert len(param) == 5, 'param should include type,M,eta,zeta,boundary as list'
+        flnm = "3D_CFD_" + str(param[0]) + "_M" + str(param[1]) + "_Eta" + str(param[2]) + '_Zeta' + str(param[3]) + "_" + str(param[4]) + "_Train.hdf5"
+        assert os.path.isfile(path + flnm), 'no such file! '+ path + flnm
+    else:
+        flnm = "3D_CFD_Rand_M0.1_Eta1e-8_Zeta1e-8_periodic_Train.hdf5"
+
+    nb = 0
+    with h5py.File(os.path.join(path, flnm), "r") as h5_file:
+        dd = np.array(h5_file["density"], dtype=np.float32)[nb]  # (batch, t, x, y, channel) --> (t, x, y, channel)
+
+    # Initialize plot
+    fig, ax = plt.subplots()
+
+    # Store the plot handle at each time step in the 'ims' list
+    ims = []
+    ax.set_title('density')
+    for i in range(dd.shape[0]):
+        im = ax.imshow(dd[i, :, :, 32].squeeze(), animated=True)
+        ims.append([im])
+
+    # Animate the plot
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+
+    writer = animation.PillowWriter(fps=15, bitrate=1800)
+    ani.save("movie.gif", writer=writer)
+    print("saved")
 
 
 def visualize_ns_incom():
     pass
 
 
-def visualize_darcy():
-    pass
+def visualize_darcy(path, param=None):
+    """
+    This function animates the Burgers equation
+
+    Args:
+    path : path to the desired file
+    param: PDE parameter to be visualized
+    """
+
+    # Read the h5 file and store the data
+    if param is not None:
+        flnm = "2D_DarcyFlow_beta" + str(param) + "_Train.hdf5"
+        assert os.path.isfile(path + flnm), 'no such file! '+ path + flnm
+    else:
+        flnm = "2D_DarcyFlow_beta1.0_Train.hdf5"
+
+    nb = 0
+    with h5py.File(os.path.join(path, flnm), "r") as h5_file:
+        data = np.array(h5_file["tensor"], dtype=np.float32)[nb]  # (batch, t, x, y, channel) --> (t, x, y, channel)
+        nu = np.array(h5_file["nu"], dtype=np.float32)[nb]  # (batch, t, x, y, channel) --> (t, x, y, channel)
+
+    # Initialize plot
+    fig, ax = plt.subplots(1, 2, figsize=(16, 8))
+
+    ax[0].imshow(data.squeeze())
+    ax[1].imshow(nu.squeeze())
+    ax[0].set_title('Data u')
+    ax[1].set_title('diffusion coefficient nu')
+    plt.savefig('2D_DarcyFlow.pdf')
+    print("saved")
 
 
-def visualize_1d_reacdiff():
-    pass
+def visualize_1d_reacdiff(path, param=None):
+    """
+    This function animates the Burgers equation
+
+    Args:
+    path : path to the desired file
+    param: PDE parameter to be visualized
+    """
+
+    # Read the h5 file and store the data
+    if param is not None:
+        assert len(param) == 2, 'param should include Nu and Rho as list'
+        flnm = "ReacDiff_Nu" + str(param[0]) + '_Rho' + str(param[1]) +".hdf5"
+        assert os.path.isfile(path + flnm), 'no such file! '+ path + flnm
+    else:
+        flnm = "ReacDiff_Nu1.0_Rho1.0.hdf5"
+
+    nb = 0
+    with h5py.File(os.path.join(path, flnm), "r") as h5_file:
+        xcrd = np.array(h5_file["x-coordinate"], dtype=np.float32)
+        data = np.array(h5_file["tensor"], dtype=np.float32)[nb]  # (batch, t, x, channel) --> (t, x, channel)
+
+    # Initialize plot
+    fig, ax = plt.subplots()
+
+    # Store the plot handle at each time step in the 'ims' list
+    ims = []
+    for i in range(data.shape[0]):
+        im,  = ax.plot(xcrd, data[i].squeeze(), animated=True)
+        if i == 0:
+            ax.plot(xcrd, data[i].squeeze())  # show an initial one first
+        ax.plot
+        ims.append([im])
+
+    # Animate the plot
+    ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+
+    writer = animation.PillowWriter(fps=15, bitrate=1800)
+    ani.save("movie.gif", writer=writer)
+    print("saved")
 
 
 if __name__ == "__main__":
@@ -204,6 +438,18 @@ if __name__ == "__main__":
         default=None,
         help="Seed number to define which sample/batch to be plotted",
     )
+    arg_parser.add_argument(
+        "--param",
+        type=float,
+        default=None,
+        help="PDE parameter to be plotted",
+    )
+    arg_parser.add_argument(
+        "--params",
+        nargs='+',
+        default=None,
+        help="PDE parameters to be plotted",
+    )
 
     args = arg_parser.parse_args()
 
@@ -213,5 +459,20 @@ if __name__ == "__main__":
         visualize_2d_reacdiff(args.data_path, args.seed_number)
     elif args.pde_name == "swe":
         visualize_swe(args.data_path, args.seed_number)
+    elif args.pde_name == "burgers":
+        visualize_burgers(args.data_path, args.param)
+    elif args.pde_name == "advection":
+        visualize_advection(args.data_path, args.param)
+    elif args.pde_name == "1d-cfd":
+        visualize_1d_cfd(args.data_path, args.params)
+    elif args.pde_name == "2d-cfd":
+        visualize_2d_cfd(args.data_path, args.params)
+    elif args.pde_name == "3d-cfd":
+        visualize_3d_cfd(args.data_path, args.params)
+    elif args.pde_name == "darcy":
+        visualize_darcy(args.data_path, args.param)
+    elif args.pde_name == "1d-reacdiff":
+        visualize_1d_reacdiff(args.data_path, args.params)
+
     else:
         raise ValueError("PDE name not recognized!")
